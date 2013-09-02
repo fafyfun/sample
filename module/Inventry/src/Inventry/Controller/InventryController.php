7ed315f86f92c70357da9968626469a3b7c7fcb0
Fawaz Jainudeen <fawazj@digitalglare.com.au>
2013-09-02 17:19:58 +0530

    Add with some issues

--- module/Inventry/src/Inventry/Controller/InventryController.php
+++ module/Inventry/src/Inventry/Controller/InventryController.php
@@ -9,8 +9,10 @@
 namespace Inventry\Controller;
 
 
+use Inventry\Model\Product;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Inventry\Form\InventryFrom;
 
 class InventryController extends AbstractActionController {
 
@@ -22,6 +24,24 @@ class InventryController extends AbstractActionController {
     }
     public function addAction(){
 
+        $form = new InventryFrom();
+        $form->get('submit')->setValue('Add');
+
+        $request= $this->getProduct();
+
+        if($request->isPost()){
+            $product = new Product();
+            $form->setInputFilter($product->getInputFilter());
+            $form->setData($request->getPost);
+
+            if($form->isValid()){
+                $product->exchangeArray($form->getData());
+                $this->getProduct()->saveProduct($product);
+
+                return $this->redirect()->toRoute('inventry');
+            }
+        }
+        return array('form'=>$form);
     }
     public function editAction(){
 
--- module/Inventry/src/Inventry/Model/Product.php
+++ module/Inventry/src/Inventry/Model/Product.php
@@ -9,11 +9,17 @@
 namespace Inventry\Model;
 
 
-class Product {
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+class Product implements InputFilterAwareInterface {
 
     public $product_id;
     public $product_name;
     public $product_description;
+    protected $inputFilter;
 
     public function exchangeArray($data){
         $this->product_id = (!empty($data['id'])) ? $data['id'] :null;
@@ -23,4 +29,78 @@ class Product {
     }
 
 
-} 
+    /**
+     * Set input filter
+     *
+     * @param  InputFilterInterface $inputFilter
+     * @return InputFilterAwareInterface
+     */
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+
+       throw new \Exception("Not Used");
+    }
+
+    /**
+     * Retrieve input filter
+     *
+     * @return InputFilterInterface
+     */
+    public function getInputFilter()
+    {
+        if(!$this->inputFilter){
+            $inputFilter = new InputFilter();
+            $factory = new InputFactory();
+
+            $inputFilter ->add($factory->createInput(array(
+                'name'=>'product_id',
+                'required'=>true,
+                'filters'=>array(
+                    array('name'=>'int')
+                )
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                    'name'=>'product_name',
+                    'required'=>true,
+                    'filters'=>array(
+                        array('name'=>'StripTags'),
+                        array('name'=>'StripTrim'),
+                    ),
+                    'validators'=>array(
+                        array(
+                            'name'=>'StringLength',
+                            'options'=>array(
+                                'encoding' => 'UTF-8',
+                                'min'=> 1,
+                                'max'=> 100,
+                            )
+                        )
+                    )
+                ))
+            );
+            $inputFilter->add($factory->createInput(array(
+                    'name'=>'product_description',
+                    'required'=>true,
+                    'filters'=>array(
+                        array('name'=>'StripTags'),
+                        array('name'=>'StripTrim'),
+                    ),
+                    'validators'=>array(
+                        array(
+                            'name'=>'StringLength',
+                            'options'=>array(
+                                'encoding' => 'UTF-8',
+                                'min'=> 1,
+                                'max'=> 100,
+                            )
+                        )
+                    )
+                ))
+            );
+
+        }
+
+        return $this->inputFilter;
+    }
+}
